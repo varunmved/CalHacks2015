6815488de51c0e6d370495eb409a9358de5901a1
Varun Ved <varunmved@gmail.com>
2015-10-09 23:53:22 -0700

    init of iOS app

---
+++ pebbleParkinsons/pebbleParkinsons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pebbleParkinsons
+//
+//  Created by Varun Ved on 10/9/15.
+//  Copyright © 2015 Varun Ved. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pebbleParkinsons/pebbleParkinsons/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pebbleParkinsons
+//
+//  Created by Varun Ved on 10/9/15.
+//  Copyright © 2015 Varun Ved. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
