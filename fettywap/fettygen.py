2d40f3f21988c404b73c895854e71209fa353197
Varun Ved <varunmved@gmail.com>
2015-10-10 04:12:33 -0700

    fixed fetty

--- fettywap/fettygen.py
+++ fettywap/fettygen.py
@@ -8,8 +8,8 @@ while True:
     if(rand1 == 17 and rand2 == 38):
         break
     else:
-        rand1 = random.randint(13,99)
-        rand2 = random.randint(13,99)
+        rand1 = random.randint(1,99)
+        rand2 = random.randint(1,99)
         count +=1
 
 print(count)
